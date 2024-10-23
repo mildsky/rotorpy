99c62d45f79184af61443f1d9aa7733f802308ef
leejun <hubo.move@gmail.com>
2024-10-23 11:46:21 +0900

    minor change

--- examples/gymnasium_basic_usage.py
+++ examples/gymnasium_basic_usage.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
 
-# For this demonstration, we'll just use the SE3 controller. 
+# For this demonstration, we'll just use the SE3 controller.
 from rotorpy.controllers.quadrotor_control import SE3Control
 from rotorpy.vehicles.crazyflie_params import quad_params
 
@@ -17,44 +17,36 @@ from rotorpy.learning.quadrotor_reward_functions import hover_reward
 # First, we need to make the gym environment. The inputs to the model are as follows...
 """
 Inputs:
-    initial_state: the initial state of the quadrotor. The default is hover. 
+    initial_state: the initial state of the quadrotor. The default is hover.
     control_mode: the appropriate control abstraction that is used by the controller, options are...
-            'cmd_motor_speeds': the controller directly commands motor speeds. 
+            'cmd_motor_speeds': the controller directly commands motor speeds.
             'cmd_motor_thrusts': the controller commands forces for each rotor.
-            'cmd_ctbr': the controller commands a collective thrsut and body rates. 
+            'cmd_ctbr': the controller commands a collective thrsut and body rates.
             'cmd_ctbm': the controller commands a collective thrust and moments on the x/y/z body axes
-            'cmd_vel': the controller commands a velocity vector in the body frame. 
-    reward_fn: the reward function, default to hover, but the user can pass in any function that is used as a reward. 
-    quad_params: the parameters for the quadrotor. 
-    max_time: the maximum time of the session. 
-    world: the world for the quadrotor to operate within. 
-    sim_rate: the simulation rate (in Hz), i.e. the timestep. 
+            'cmd_vel': the controller commands a velocity vector in the body frame.
+    reward_fn: the reward function, default to hover, but the user can pass in any function that is used as a reward.
+    quad_params: the parameters for the quadrotor.
+    max_time: the maximum time of the session.
+    world: the world for the quadrotor to operate within.
+    sim_rate: the simulation rate (in Hz), i.e. the timestep.
     render_mode: render the quadrotor.
             'None': no rendering
-            'console': output text describing the environment. 
-            '3D': will render the quadrotor in 3D. WARNING: THIS IS SLOW. 
+            'console': output text describing the environment.
+            '3D': will render the quadrotor in 3D. WARNING: THIS IS SLOW.
 
 """
-# env = gym.make("Quadrotor-v0", 
-#                 control_mode ='cmd_motor_speeds', 
-#                 reward_fn = hover_reward,
-#                 quad_params = quad_params,
-#                 max_time = 5,
-#                 world = None,
-#                 sim_rate = 100,
-#                 render_mode='3D',
-#                 render_fps=30)
+# env = gym.make("Quadrotor-v0", control_mode ='cmd_motor_speeds', reward_fn = hover_reward,quad_params = quad_params,max_time = 5,world = None,sim_rate = 100,render_mode='3D',render_fps=30)
 env = QuadrotorEnv(control_mode ='cmd_motor_speeds', reward_fn=hover_reward, quad_params=quad_params, max_time=5, render_mode='3D')
 
 # Now reset the quadrotor.
 # Setting initial_state to 'random' will randomly place the vehicle in the map near the origin.
-# But you can also set the environment resetting to be deterministic. 
+# But you can also set the environment resetting to be deterministic.
 observation, info = env.reset(initial_state='random')
 
 # Number of timesteps
 T = 300
 time = np.arange(T)*(1/100)      # Just for plotting purposes.
-position = np.zeros((T, 3))      # Just for plotting purposes. 
+position = np.zeros((T, 3))      # Just for plotting purposes.
 velocity = np.zeros((T, 3))      # Just for plotting purposes.
 reward_sum = np.zeros((T,))      # Just for plotting purposes.
 actions = np.zeros((T, 4))       # Just for plotting purposes.
@@ -67,12 +59,12 @@ for i in range(T):
     state = {'x': observation[0:3], 'v': observation[3:6], 'q': observation[6:10], 'w': observation[10:13]}
     
     # For illustrative purposes, just command the quad to hover.
-    flat = {'x': [0, 0, 0], 
-            'x_dot': [0, 0, 0], 
-            'x_ddot': [0, 0, 0], 
+    flat = {'x': [0, 0, 0],
+            'x_dot': [0, 0, 0],
+            'x_ddot': [0, 0, 0],
             'x_dddot': [0, 0, 0],
-            'yaw': 0, 
-            'yaw_dot': 0, 
+            'yaw': 0,
+            'yaw_dot': 0,
             'yaw_ddot': 0}
     control_dict = controller.update(0, state, flat)
 
